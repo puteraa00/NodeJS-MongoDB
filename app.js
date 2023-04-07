eed252e3ecf9b74ea420019d12c15d946899c269
Putra Dwi <putraadekaa@gmail.com>
2023-04-07 13:28:29 +0700

    New Projek

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const  PORT = process.env.PORT || 3000;
+
 
 const app = express();
 
@@ -132,6 +134,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, function() {
+  console.log("Server started on port " + PORT);
 });
