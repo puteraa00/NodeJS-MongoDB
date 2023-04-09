1e6ea32992a82427d7f4a3ac6129ed6127185bb4
Putra Dwi <putraadekaa@gmail.com>
2023-04-09 10:21:38 +0700

    indexxxxxx

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 mongoose.set('strictQuery', false);
-async function connectDB(){
+const connectDB = async()=>{
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser : true, useUnifiedTopology: true});
     console.log(`MongoDB Connect :  ${conn.connection.host}`);
